ee409250d8c33095316c315c448c33c6cf58d648
Seoyoung Jo <josy2665@gmail.com>
2021-08-10 14:00:11 +0900

    comment: printer 문제 관련 주석 추가

--- stack-queue/printer.js
+++ stack-queue/printer.js
@@ -7,23 +7,26 @@ function solution(priorities, location) {
   let count = 0;
 
   while (priorities.length) {
+    // 맨앞 작업을 빼낸다.
     let first = priorities.shift();
 
-    // console.log(location, count);
-    //  더 중요한 문서가 있다면
+    // 맨앞 작업보다 더 중요한 문서가 있다면
     if (priorities.filter((p) => p > first).length) {
       priorities.push(first);
     } else {
+      // count (인쇄 횟수) 1 증가
       count++;
+      // 내가 인쇄를 요청한 문서라면, 해당 문서의 횟수 (count)를 answer에 넣고 while문 종료
       if (location == 0) {
         answer = count;
         break;
       }
     }
-    // 맨 뒤로 갔을 때
+    // 내가 요청한 문서가 맨 뒤로 갔을 때 location 위치를 length - 1 (맨뒤 인덱스)로 초기화
     if (location == 0) {
       location = priorities.length - 1;
     } else {
+      // 요청한 문서 인덱스 값 1 감소 (계속해서 맨앞의 값이 줄어들기 때문에)
       location--;
     }
   }
